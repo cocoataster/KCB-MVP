872502bac78b6b71f5223c7f0977d7be9694913e
cocoataster <cocoataster@protonmail.ch>
2019-11-20 21:36:38 +0100

    Add: Locator, base model and base view implementing Provider

---
+++ lib/screens/views/base_view.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:sounds_good/core/viewmodels/base_model.dart';
+import 'package:sounds_good/locator.dart';
+
+class BaseView<T extends BaseModel> extends StatefulWidget {
+  final Widget Function(BuildContext context, T model, Widget child) builder;
+  final Function(T) onModelReady;
+
+  BaseView({this.builder, this.onModelReady});
+
+  @override
+  _BaseViewState<T> createState() => _BaseViewState<T>();
+}
+
+class _BaseViewState<T extends BaseModel> extends State<BaseView<T>> {
+  T model = locator<T>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.onModelReady != null) {
+      widget.onModelReady(model);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<T>(
+        builder: (context) => model,
+        child: Consumer<T>(builder: widget.builder));
+  }
+}
