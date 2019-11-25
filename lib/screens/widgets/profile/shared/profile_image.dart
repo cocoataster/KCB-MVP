4b9183e9ec774e11300ae735623ae2216446f827
Gabi Lago Blasco <gabi@MacBook-Pro-de-Gabi.local>
2019-11-25 10:11:43 +0100

    Profile photo now renders from server

--- lib/screens/widgets/profile/shared/profile_image.dart
+++ lib/screens/widgets/profile/shared/profile_image.dart
@@ -1,26 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sounds_good/core/viewmodels/profile_viewmodel.dart';
+import 'package:sounds_good/core/services/api.dart';
 
 class ProfileImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 20.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Card(
-            elevation: 6.0,
-            clipBehavior: Clip.antiAlias,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(15.0),
+    return Consumer<ProfileViewModel>(
+      builder: (context, model, child) => Padding(
+        padding: const EdgeInsets.only(top: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Card(
+              elevation: 6.0,
+              clipBehavior: Clip.antiAlias,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Image.network(
+                '${Api.endpoint}/${model.profile.photo}',
+                fit: BoxFit.cover,
+              ),
             ),
-            child: Image.asset(
-              'assets/images/profile.png',
-              fit: BoxFit.cover,
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
