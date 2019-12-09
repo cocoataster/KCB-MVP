9e3fac834d5455df51e912d8218cf4899413c67a
Gabi Lago Blasco <gabi@MacBook-Pro-de-Gabi.local>
2019-12-09 11:28:36 +0100

    Location fromJson forced to return Double

--- lib/core/models/location.dart
+++ lib/core/models/location.dart
@@ -1,18 +1,18 @@
 class Location {
-  final double lat;
-  final double long;
+  double lat;
+  double long;
 
   Location({this.lat, this.long});
 
-  factory Location.fromJson(Map<String, dynamic> json) {
-    return Location(lat: json['lat'], long: json['long']);
-  }
+   factory Location.fromJson(Map<String, dynamic> json) {
+    
+    return Location(lat: json['lat'].toDouble(), long: json['long'].toDouble());
+  } 
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = Map<String, dynamic>();
     data['lat'] = this.lat;
     data['long'] = this.long;
-
     return data;
   }
 }
